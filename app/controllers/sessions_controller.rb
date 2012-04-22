2e86f0a226363c862eedf20f058016f0f9fbe107
Ralphos <wintle.ralph@gmail.com>
2012-04-22 11:29:06 -0500

    Set up storing of User Authentication Data in User object

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
   def create
-    raise request.env["omniauth.auth"].to_yaml
+    auth = request.env["omniauth.auth"]
+    user = User.where(:provider => auth['provider'],
+                     :uid => auth['uid']).first || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Signed in!"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
   attr_protected :email, :name, :provider, :uid
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+        user.name = auth['info']['name'] || ""
+        user.email = auth['info']['name'] || ""
+      end
+    end
+  end
 end
