8e2c7bc593aa42c0e3089947042d7059bc15ca95
Ralphos <wintle.ralph@gmail.com>
2012-04-22 11:38:22 -0500

    Added signin/signout actions to sessions controller and helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,9 @@
 class SessionsController < ApplicationController
+
+  def new
+    redirect_to '/auth/facebook' 
+  end
+
   def create
     auth = request.env["omniauth.auth"]
     user = User.where(:provider => auth['provider'],
@@ -6,4 +11,15 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     redirect_to root_url, :notice => "Signed in!"
   end
+
+  def failure
+    redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
+    
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url, :notice => 'Signed out!'
+  end
+
 end
