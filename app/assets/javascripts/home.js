a8f6aad5a25633d9dbfc9e3e8d2a50f31ef1852b
Ralphos <wintle.ralph@gmail.com>
2012-04-26 15:51:28 -0500

    Trying some new javascript

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,15 @@
-// 
+$('input.fb_connect').click(function(e) {
+var width = 600, height = 400;
+var left = (screen.width / 2) - (width / 2);
+var top = (screen.height / 2) - (2 * height / 3);
+var features = 'menubar=no,toolbar=no,status=no,width=' + width + ',height=' + height + ',toolbar=no,left=' + left + ',top=' + top;
+var loginWindow = window.open('/users/auth/facebook', '_blank', features);
+loginWindow.focus();
+e.preventDefault();
+return false;
+});
+
+ 
 // 
 // window.fbAsyncInit = function() {
 //      FB.init({
